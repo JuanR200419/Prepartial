2a1e2ac1a27748a4045fc6498698f105332d7a2a
JuanR200419 <kedipert27@gmail.com>
2024-03-01 22:36:37 -0500

    Feat(add code new):create new code for register a student commit two in new branch

---
+++ PrepartialProject/build/classes/Conexion/mySqualTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Student (
+    identification VARCHAR(50) PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    degree VARCHAR(50) NOT NULL,
+    subject VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+
+
